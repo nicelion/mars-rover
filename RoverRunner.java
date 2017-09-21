2a08bf86f6284b3a7b5eafc0683f74a46aae19d3
Ian Thompson <ianthompson@nicelion.com>
2017-09-21 10:28:33 -0400

    added Sojourner rover

--- RoverRunner.java
+++ RoverRunner.java
@@ -11,6 +11,7 @@ public class RoverRunner
     {
         Rover r1 = new Rover("Curiosity");
         Rover r2 = new Rover("Spirit");
+        Rover r3 = new Rover("Sojourner");
         
         System.out.println(r1);
         
@@ -25,6 +26,12 @@ public class RoverRunner
         
         r2.move();
         System.out.println(r2);
+        
+        r3.rotateLeft();
+        System.out.println(r3);
+        
+        r3.move();
+        System.out.println(r3);
         
         r1.move();
         System.out.println(r1);
